ff2330e2b4181face66be9677b3a6dd6c26c1459
PrinkalHarsh <priarsh151@gmail.com>
2020-05-20 13:15:35 +0530

    initial commit

--- App.js
+++ App.js
@@ -1,19 +1,16 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
-export default function App() {
+// import WelcomeScreen from '@screens';
+
+
+
+function WelcomeScreen() {
   return (
-    <View style={styles.container}>
+    <View>
       <Text>Open up App.js to start working on your app!</Text>
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default WelcomeScreen;
